e99fed2d16a6e4ac95e8848cf449ef80a022e4d3
Daniel Larimer <dan@fractally.com>
2026-04-30 04:28:23 -0500

    Build psitricorder in Psitri Silkworm checkout

--- conanfile.py
+++ conanfile.py
@@ -109,6 +109,7 @@ class SilkwormRecipe(ConanFile):
 
             # silkworm requires:
             'iostreams',
+            'program_options',
             'system',
             'thread',
             'url',
